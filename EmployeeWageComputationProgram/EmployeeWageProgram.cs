440f27bf1db801938ec6425f3077c99c662379d7
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-01-28 10:27:09 +0530

    [Ashish] :Employee Wage Problem Solved

--- EmployeeWageComputationProgram/EmployeeWageProgram.cs
+++ EmployeeWageComputationProgram/EmployeeWageProgram.cs
@@ -10,12 +10,14 @@ namespace EmployeeWageComputationProgram
     {
         public void Attendence()
         {
-            const int PRESENT = 1, ABSENT = 0;
-            Random random = new Random();
-            int check = random.Next(0, 2);
+            const int PRESENT = 1, ABSENT = 0,WAGE_PER_HOUR=20, FULL_DAY_HOUR=8;
+            Random random = new Random(); 
+           int check = random.Next(0, 2);
             if (check == PRESENT)
             {
                 Console.WriteLine("Employee is Present \n");
+                int dailywage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                Console.WriteLine("The Daily Wage Of the Employee is : {0}", dailywage);
             }
             if (check == ABSENT)
             {
