3ca0c3c1c69f3553ee148ed48c7a3b172696db10
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-01-28 10:46:53 +0530

    [Ashish] Add:Monthly Wage Problem solved

--- EmployeeWageComputationProgram/EmployeeWageProgram.cs
+++ EmployeeWageComputationProgram/EmployeeWageProgram.cs
@@ -10,24 +10,26 @@ namespace EmployeeWageComputationProgram
     {
         public void Attendence()
         {
-            const int PART_TIME = 0, FULL_TIME = 1, ABSENT = 0, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8, PART_TIME_HOUR = 4;
-            int dailywage = 0;
+            const int PART_TIME = 0, FULL_TIME = 1, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8, PART_TIME_HOUR = 4, WORKING_DAYS = 20;
+            int monthlywage = 0;
             Random random = new Random();
-            int check = random.Next(0, 3);
-            switch (check)
+            for (int i = 0; i < WORKING_DAYS; i++)
             {
-                case PART_TIME:
-                    dailywage += WAGE_PER_HOUR * PART_TIME_HOUR;
-                    break;
-                case FULL_TIME:
-                    dailywage += WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    break;
-                default:
-                    dailywage = 0;
-                    break;
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case PART_TIME:
+                        monthlywage += WAGE_PER_HOUR * PART_TIME_HOUR;
+                        break;
+                    case FULL_TIME:
+                        monthlywage += WAGE_PER_HOUR * FULL_DAY_HOUR;
+                        break;
+                    default:
+                        monthlywage += 0;
+                        break;
+                }
             }
-
-            Console.WriteLine("The today Wage Of the Employee is :{0}", dailywage);
+            Console.WriteLine("The Monthly Wage Of the Employee is :{0}", monthlywage);
 
         }
 
