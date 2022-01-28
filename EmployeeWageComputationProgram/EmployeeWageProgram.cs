5bcaf946a6d77587e36c10d6f0cc75321fa0878f
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-01-28 10:52:43 +0530

    [Ashish] Add:Monthly wage problem solved Including condition

--- EmployeeWageComputationProgram/EmployeeWageProgram.cs
+++ EmployeeWageComputationProgram/EmployeeWageProgram.cs
@@ -10,26 +10,29 @@ namespace EmployeeWageComputationProgram
     {
         public void Attendence()
         {
-            const int PART_TIME = 0, FULL_TIME = 1, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8, PART_TIME_HOUR = 4, WORKING_DAYS = 20;
+            const int PART_TIME = 0, FULL_TIME = 1, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8, PART_TIME_HOUR = 4, WORKING_DAYS = 20,MAX_WORK_HOUR=100;
             int monthlywage = 0;
+            int workinghour = 0;
             Random random = new Random();
-            for (int i = 0; i < WORKING_DAYS; i++)
+            for (int i = 0; i < WORKING_DAYS&&MAX_WORK_HOUR>=workinghour; i++)
             {
                 int check = random.Next(0, 3);
                 switch (check)
                 {
                     case PART_TIME:
                         monthlywage += WAGE_PER_HOUR * PART_TIME_HOUR;
+                        workinghour += PART_TIME_HOUR;
                         break;
                     case FULL_TIME:
                         monthlywage += WAGE_PER_HOUR * FULL_DAY_HOUR;
+                        workinghour += FULL_DAY_HOUR;
                         break;
                     default:
                         monthlywage += 0;
                         break;
                 }
             }
-            Console.WriteLine("The Monthly Wage Of the Employee is :{0}", monthlywage);
+            Console.WriteLine("The Monthly Wage Of the Employee is :{0} and Total Working hour was {1}", monthlywage,workinghour);
 
         }
 
