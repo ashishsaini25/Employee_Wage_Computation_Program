0151853f49ade01c556848e4c0cc046e150460ae
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-07 23:10:59 +0530

    [Ashish] Refactor:Wages of multiple company facility added

--- EmployeeWageComputationProgram/EmployeeWageProgram.cs
+++ EmployeeWageComputationProgram/EmployeeWageProgram.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,33 +9,44 @@ namespace EmployeeWageComputationProgram
 {
     public class EmployeeWageProgram
     {
-        public void Attendence()
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public void Attendence(int WAGE_PER_HOUR, int WORKING_DAYS, int MAX_WORK_HOUR)
         {
-            const int PART_TIME = 0, FULL_TIME = 1, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8, PART_TIME_HOUR = 4, WORKING_DAYS = 20,MAX_WORK_HOUR=100;
+
+            const int PART_TIME = 0, FULL_TIME = 1, FULL_DAY_HOUR = 8, PART_TIME_HOUR = 4;
             int monthlywage = 0;
             int workinghour = 0;
             Random random = new Random();
-            for (int i = 0; i < WORKING_DAYS&&MAX_WORK_HOUR>=workinghour; i++)
+            try
             {
-                int check = random.Next(0, 3);
-                switch (check)
-                {
-                    case PART_TIME:
-                        monthlywage += WAGE_PER_HOUR * PART_TIME_HOUR;
-                        workinghour += PART_TIME_HOUR;
-                        break;
-                    case FULL_TIME:
-                        monthlywage += WAGE_PER_HOUR * FULL_DAY_HOUR;
-                        workinghour += FULL_DAY_HOUR;
-                        break;
-                    default:
-                        monthlywage += 0;
-                        break;
-                }
-            }
-            Console.WriteLine("The Monthly Wage Of the Employee is :{0} and Total Working hour was {1}", monthlywage,workinghour);
 
-        }
+
+                for (int i = 0; i < WORKING_DAYS && MAX_WORK_HOUR >= workinghour; i++)
+                {
+                    int check = random.Next(0, 3);
+                    switch (check)
+                    {
+                        case PART_TIME:
+                            monthlywage += WAGE_PER_HOUR * PART_TIME_HOUR;
+                            workinghour += PART_TIME_HOUR;
+                            break;
+                        case FULL_TIME:
+                            monthlywage += WAGE_PER_HOUR * FULL_DAY_HOUR;
+                            workinghour += FULL_DAY_HOUR;
+                            break;
+                        default:
+                            monthlywage += 0;
+                            break;
+                    }
+                }
+                Console.WriteLine("The Monthly Wage Of the Employee is :{0} and Total Working hour was {1}", monthlywage, workinghour);
+                logger.Info("Logger file added\n");
+            }
+            catch (Exception Ashish)
+            {
+                logger.Error(Ashish);
+            }
+            }
 
     }
 }
--- EmployeeWageComputationProgram/Program.cs
+++ EmployeeWageComputationProgram/Program.cs
@@ -10,8 +10,14 @@ namespace EmployeeWageComputationProgram
     {
         static void Main(string[] args)
         {
-          EmployeeWageProgram employee = new EmployeeWageProgram();
-            employee.Attendence();
+          EmployeeWageProgram company1 = new EmployeeWageProgram();
+            company1.Attendence(100,25,100);
+            EmployeeWageProgram company2 = new EmployeeWageProgram();
+            company2.Attendence(100, 25, 50);
+            EmployeeWageProgram company3 = new EmployeeWageProgram();
+            company3.Attendence(90, 25, 200);
+            EmployeeWageProgram company4 = new EmployeeWageProgram();
+            company4.Attendence(100, 25, 100);
 
         }
     }
